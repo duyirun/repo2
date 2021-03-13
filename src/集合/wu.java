5f99138223153cf8c4e0c279cec6db9134b57db3
杜某人 <2531198115@qq.com>
2021-03-13 20:17:25 +0800

    去掉两个无用的

--- src/集合/wu.java
+++ src/集合/wu.java
@@ -5,11 +5,12 @@ import sun.rmi.log.LogInputStream;
 import java.util.List;
 import java.util.LinkedList;
 
-public class LinkedListDemo {
+public class wu {
   LinkedList ll=new LinkedList();
     student s1=new student("张三",18);
     student s2=new student("李四",19);
     student s3=new student("王五",20);
+
 
 
 }
